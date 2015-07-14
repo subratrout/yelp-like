82c42d3c45906fd74f8eb507b367cc96ad7f97e8
Subrat Rout <subratnrout@gmail.com>
2015-07-13 19:49:51 -0700

    Added User and Admin permissions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :set_restaurant
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   def index
     @reviews = Review.all
@@ -45,12 +46,17 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to restaurant_path(@restaurant), notice: 'Review was successfully deleted.' }
       format.json { head :no_content }
     end
   end
 
   private
+    def check_user
+      unless (@review.user == current_user) || (current_user.admin?)
+        redirect_to root_url, alert: "Sorry, this review belongs to someone else"
+      end
+    end
     def set_review
       @review = Review.find(params[:id])
     end
